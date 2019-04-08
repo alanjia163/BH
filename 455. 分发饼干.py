44bedeb0b0ec81782f9323301917310bdfe3a017
jiashilin <38239304+alanjia163@users.noreply.github.com>
2019-04-08 15:16:49 +0800

    455

---
+++ 455. 分发饼干.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+# Author: Jia ShiLin
+
+def findContentChildren(g, s):
+    count = 0
+    g.sort()
+    s.sort()
+    t=0
+    for i in range(len(s)):
+        for j in range(t,len(g)):
+            if g[j] <= s[i]:
+                count += 1
+                t=j+1
+                break
+    return count
+
+if __name__ == '__main__':
+    print(findContentChildren([10,9,8,7],[5,6,7,8]))
